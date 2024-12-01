d9a040318bc68869c6642c8834ae2aef4a998f4d
pacojfrn <124843876+pacojfrn@users.noreply.github.com>
2024-11-30 18:53:55 -0600

    Soap kubernetes funcional
    Actualizacion Soap con kubernetes funcional

--- Proyecto-final/Soap/Program.cs
+++ Proyecto-final/Soap/Program.cs
@@ -6,23 +6,7 @@ using SoapCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Intenta obtener la cadena de conexión de las variables de entorno primero
-var connectionString = Environment.GetEnvironmentVariable("ConnectionString__DefaultConnection");
 
-// Si no se encuentra, obtén el valor desde appsettings.json
-if (string.IsNullOrEmpty(connectionString))
-{
-    connectionString = builder.Configuration.GetSection("MongoDb:Persona:ConnectionString").Value;
-}
-
-if (string.IsNullOrEmpty(connectionString))
-{
-    throw new ArgumentNullException("La cadena de conexión de MongoDB no puede estar vacía.");
-}
-
-Console.WriteLine($"Usando cadena de conexión: {connectionString}");
-
-// Configura el cliente de MongoDB
 builder.Services.AddSingleton<IMongoClient, MongoClient>(s => new MongoClient(builder.Configuration.GetValue<string>("MongoDb:Persona:ConnectionString")));
 
 // Registra repositorios y servicios
